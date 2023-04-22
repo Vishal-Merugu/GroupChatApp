e26c81e85cc8310940c956e52340e1ce24a8cfa1
Vishal-Merugu <buntymerugu0@gmail.com>
2023-04-22 20:04:01 +0530

    signup UI

--- Frontend/signup/signup.js
+++ Frontend/signup/signup.js
@@ -1,17 +1,45 @@
+const url = "http://localhost:3000"
+
+
 const Name = document.querySelector("#name");
 const Email = document.querySelector("#email");
 const Phone = document.querySelector("#phone");
 const Password = document.querySelector("#password")
+const alertu = document.querySelector('.alertu')
+
 
 document.addEventListener("DOMContentLoaded", () => {
-    document.querySelector('form').onsubmit = (e) => {
-        e.preventDefault()
-
-        const name = Name.value;
-        const email = Email.value;
-        const phone = Phone.value ;
-        const password = Password.value ;
-
-        console.log(name, email, phone, password);
+    document.querySelector('form').onsubmit = async (e) => {
+        try{
+            e.preventDefault()
+    
+            const name = Name.value;
+            const email = Email.value;
+            const phone = Phone.value ;
+            const password = Password.value ;
+    
+            const user = {
+                name : name,
+                email : email,
+                phone : phone,
+                password : password
+            }
+    
+            const response = await axios.post(`${url}/user/signup`, user)
+            console.log(response);
+    
+            if(response.status == 200){
+                alert("Account Created Successfully");   
+            } 
+            else{
+                throw new Error("User Already Exists")
+            }  
+        }
+        catch(err){
+            alertu.innerHTML = `<div class="alert alert-danger alert-dismissible fade in">
+            <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>${err.response.data.message}
+            </div>`
+            setTimeout(()=> document.querySelector('.alert').remove(),3000)
+        }
     }
 })
